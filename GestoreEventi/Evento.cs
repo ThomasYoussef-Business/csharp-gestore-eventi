39aa3a4ccf4a51de40b8390ae08ec335c6d2e501
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-12-13 10:35:12 +0100

    Aggiunto costruttore per classe Evento

--- GestoreEventi/Evento.cs
+++ GestoreEventi/Evento.cs
@@ -5,7 +5,7 @@
  *          [x] Data - r/w
  *          [x] Capienza massima - r/
  *          [x] Posti prenotati - r/
- *          [] Costruttore che prende titolo, data, e capienza massima
+ *          [x] Costruttore che prende titolo, data, e capienza massima
  *          [] PrenotaPosti()
  *          [] DisdiciPosti()
  *          [] override ToString
@@ -18,7 +18,7 @@ namespace GestoreEventi {
         // ATTRIBUTI
         private string titolo;
         private DateOnly data;
-        private uint capienzaMassima;
+        private readonly uint capienzaMassima;
         private uint postiPrenotati;
 
         // PROPRIETÀ
@@ -40,10 +40,32 @@ namespace GestoreEventi {
                 data = value;
             }
         }
-        public uint CapienzaMassima { get => capienzaMassima; init => capienzaMassima = value; }
-        public uint PostiPrenotati { get => postiPrenotati; protected set => postiPrenotati = value; }
+        public uint CapienzaMassima {
+            get => capienzaMassima;
+            init {
+                if (value < 1) {
+                    throw new ArgumentOutOfRangeException(nameof(value), $"Capienza Massima {nameof(value)} non può essere zero o meno.");
+                }
+                capienzaMassima = value;
+            }
+        }
+        public uint PostiPrenotati {
+            get => postiPrenotati;
+            protected set {
+                if (value > CapienzaMassima) {
+                    throw new ArgumentOutOfRangeException(nameof(value), $"Posti prenotati {nameof(value)} non può superare la capienza massima.");
+                }
+                postiPrenotati = value;
+            }
+        }
 
         // COSTRUTTORI
+        public Evento(string titolo, DateTime data, uint capienzaMassima) {
+            Titolo = titolo;
+            Data = DateOnly.FromDateTime(data);
+            CapienzaMassima = capienzaMassima;
+            PostiPrenotati = 0;
+        }
 
 
         // METODI PUBBLICI
