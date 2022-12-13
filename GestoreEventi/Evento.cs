0b5a7363ecf99ea23c29c2cf754659bfc1937686
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-12-13 10:26:55 +0100

    Aggiunti attributi e proprietà necessarie ad Evento

--- GestoreEventi/Evento.cs
+++ GestoreEventi/Evento.cs
@@ -1,10 +1,10 @@
 ﻿/*
  * Milestone 1:
  *      [] Classe evento con:
- *          [] Titolo - r/w
- *          [] Data - r/w
- *          [] Capienza massima - r/
- *          [] Posti prenotati - r/
+ *          [x] Titolo - r/w
+ *          [x] Data - r/w
+ *          [x] Capienza massima - r/
+ *          [x] Posti prenotati - r/
  *          [] Costruttore che prende titolo, data, e capienza massima
  *          [] PrenotaPosti()
  *          [] DisdiciPosti()
@@ -14,19 +14,42 @@
 
 namespace GestoreEventi {
     public class Evento {
-		// ATTRIBUTI
+
+        // ATTRIBUTI
+        private string titolo;
+        private DateOnly data;
+        private uint capienzaMassima;
+        private uint postiPrenotati;
+
+        // PROPRIETÀ
+        public string Titolo {
+            get => titolo;
+            set {
+                if (string.IsNullOrWhiteSpace(value)) {
+                    throw new ArgumentNullException(nameof(value), $"Titolo {nameof(value)} non può essere nullo o vuoto.");
+                }
+                titolo = value;
+            }
+        }
+        public DateOnly Data {
+            get => data;
+            set {
+                if (value.CompareTo(DateTime.Now) < 0) {
+                    throw new ArgumentOutOfRangeException(nameof(value), $"Data {nameof(value)} non può essere nel passato.");
+                }
+                data = value;
+            }
+        }
+        public uint CapienzaMassima { get => capienzaMassima; init => capienzaMassima = value; }
+        public uint PostiPrenotati { get => postiPrenotati; protected set => postiPrenotati = value; }
+
+        // COSTRUTTORI
 
 
-		// PROPRIETÀ
+        // METODI PUBBLICI
 
 
-		// COSTRUTTORI
+        // METODI PRIVATI
 
-
-		// METODI PUBBLICI
-
-
-		// METODI PRIVATI
-
-	}
+    }
 }
